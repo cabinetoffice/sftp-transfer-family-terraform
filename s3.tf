48b55df9fd19e415f8861bf115dd2e05d3df01dc
co-pm <80323133+co-pm@users.noreply.github.com>
2024-03-17 10:59:45 +0000

    Add files via upload

---
+++ s3.tf
@@ -0,0 +1,14 @@
+resource "aws_s3_bucket" "sftp" {
+  bucket_prefix   = var.s3_bucket_name
+  versioning {
+    enabled       = try(var.s3_bucket_versioning, true)
+  }
+  server_side_encryption_configuration {
+    rule {
+      bucket_key_enabled = false
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
