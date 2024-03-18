17fd20deb6f3cce661261115bffef3806ccebfcb
co-pm <80323133+co-pm@users.noreply.github.com>
2024-03-18 17:41:35 +0000

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -51,7 +51,7 @@ data "aws_iam_policy_document" "s3_access_for_sftp_users" {
     ]
 
     resources = [
-      "${join("", data.aws_s3_bucket.landing[*].arn)}/${each.value.username}/*"
+      "${join("", data.aws_s3_bucket.landing[*].arn)}/*"
     ]
   }
 }
